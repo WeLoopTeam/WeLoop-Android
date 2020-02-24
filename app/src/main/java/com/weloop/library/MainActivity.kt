a1993fbb76863d7e4d37119531066d70bd3f44b4
Thauvin Alexandre <alexandre.thauvin01@gmail.com>
2020-02-24 18:10:52 +0100

    shake is implemented, all 3 ways to invoke are working smoothly

--- app/src/main/java/com/weloop/library/MainActivity.kt
+++ app/src/main/java/com/weloop/library/MainActivity.kt
@@ -1,18 +1,59 @@
 package com.weloop.library
 
+import android.hardware.SensorEventListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
+import android.view.View
+import com.google.android.material.tabs.TabLayout
 import com.weloop.weloop.FloatingWidget
-import com.weloop.weloop.WeLoop
+import com.weloop.weloop.WeLoopWebView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var weLoopWebView: WeLoopWebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val webView = findViewById<WeLoop>(R.id.webview)
-        webView.initialize("742382b0-531e-11ea-8733-0fb1656485aa", findViewById<FloatingWidget>(R.id.fab))
+        weLoopWebView = findViewById(R.id.webview)
+        weLoopWebView.initialize("742382b0-531e-11ea-8733-0fb1656485aa", findViewById<FloatingWidget>(R.id.fab))
+        initListeners()
+        tabs.getTabAt(2)!!.select()
+    }
+
+    private fun initListeners(){
+        tvManualInvocation.setOnClickListener {
+
+            if (tabs.selectedTabPosition == 0) {
+                weLoopWebView.invoke()
+            }
+        }
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                when (p0!!.position) {
+                    0 -> weLoopWebView.setInvocationMethod(WeLoopWebView.MANUAL)
+                    1 -> weLoopWebView.setInvocationMethod(WeLoopWebView.SHAKE_GESTURE)
+                    2 -> weLoopWebView.setInvocationMethod(WeLoopWebView.FAB)
+                }
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+            }
+        })
+    }
+
+    override fun onBackPressed() {
+        if (webview.visibility == View.VISIBLE){
+            webview.visibility = View.GONE
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
--- weloop/src/main/java/com/weloop/weloop/WeLoopWebView.kt
+++ weloop/src/main/java/com/weloop/weloop/WeLoopWebView.kt
@@ -3,14 +3,17 @@ package com.weloop.weloop
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import android.webkit.WebView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.marginBottom
-import androidx.core.view.setMargins
+import com.github.tbouron.shakedetector.library.ShakeDetector
+import com.github.tbouron.shakedetector.library.ShakeDetector.OnShakeListener
 import com.weloop.weloop.network.ApiServiceImp
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -19,10 +22,12 @@ import io.reactivex.schedulers.Schedulers
 
 /* Created by *-----* Alexandre Thauvin *-----* */
 
-class WeLoop : WebView {
+class WeLoopWebView : WebView {
     private var currentInvocationMethod = 0
     private var apiKey: String = ""
     private lateinit var floatingWidget: FloatingWidget
+    private lateinit var sensorMgr: SensorManager
+    private lateinit var sensor: Sensor
     private val disposable = CompositeDisposable()
 
     constructor(context: Context) : super(context)
@@ -30,15 +35,22 @@ class WeLoop : WebView {
 
     init {
         visibility = View.GONE
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.javaScriptEnabled = true
     }
 
 
     fun initialize(apiKey: String, floatingWidget: FloatingWidget) {
+        ShakeDetector.create(context, OnShakeListener{
+            invoke()
+        })
+
         this.floatingWidget = floatingWidget
         this.floatingWidget.setOnClickListener {
-            this.visibility = View.VISIBLE
-            loadUrl("")
+            invoke()
         }
+
+        loadUrl(URL + apiKey)
         this.apiKey = apiKey
         disposable.add(ApiServiceImp.getWidgetPreferences(this.apiKey)
             .subscribeOn(Schedulers.io())
@@ -87,35 +99,52 @@ class WeLoop : WebView {
 
     fun setInvocationMethod(invocationMethod: Int) {
         this.currentInvocationMethod = invocationMethod
+        renderInvocation()
     }
 
     private fun renderInvocation() {
-        if (::floatingWidget.isInitialized) {
-            floatingWidget.visibility = View.GONE
-        }
         when (currentInvocationMethod) {
             FAB -> {
                 if (::floatingWidget.isInitialized) {
                     floatingWidget.visibility = View.VISIBLE
                 }
+                ShakeDetector.stop()
             }
             SHAKE_GESTURE -> {
-
+                if (::floatingWidget.isInitialized) {
+                    floatingWidget.visibility = View.GONE
+                }
+                ShakeDetector.start()
             }
             else -> {
-
+                if (::floatingWidget.isInitialized) {
+                    floatingWidget.visibility = View.GONE
+                }
+                ShakeDetector.stop()
             }
         }
     }
 
-    fun invoke(url: String) {
+    fun resumeWeLoop(){
+        ShakeDetector.start()
+    }
+
+    fun stopWeLoop(){
+        ShakeDetector.stop()
+    }
+
+    fun destroyWeLoop(){
+        ShakeDetector.destroy()
+    }
+
+    fun invoke() {
         visibility = View.VISIBLE
-        loadUrl(url)
     }
 
     companion object {
         const val FAB = 0
         const val SHAKE_GESTURE = 1
         const val MANUAL = 2
+        private const val URL = "https://staging-widget.30kg-rice.cooking/home?appGuid="
     }
 }
